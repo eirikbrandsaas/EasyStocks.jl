dcd24ba91372ce043c3dcf97e15f794da0a81e79
Eirik Eylands Brandsaas <eirik.e.brandsaas@frb.gov>
2021-10-26 14:12:36 -0400

    Started working

--- src/EasyStocks.jl
+++ src/EasyStocks.jl
@@ -1,5 +1,10 @@
 module EasyStocks
 
 # Write your package code here.
+function SolveModel()
+  println("testing")
+  α = fill(0.0,10,10)
+end
 
+export SolveModel
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "EasyStocks.jl" begin
     # Write your tests here.
+    @test minimum(SolveModel())<1.0
 end
