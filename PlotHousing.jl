0ee76655cd756b20962acf7103ddc77d71a5202d
Eirik Eylands Brandsaas <eirik.e.brandsaas@frb.gov>
2021-11-05 15:25:54 -0400

    Updated housing plots

--- PlotHousing.jl
+++ PlotHousing.jl
@@ -7,9 +7,9 @@ include("src/EasyStocks.jl")
 ## Setup
 using CairoMakie # For plotting
 
-mp1 = ModPar(q=0.00,ψ=0.015,xstar=.0,η=0.5)
+mp1 = ModPar(q=0.005,ψ=0.015,xstar=.0,η=0.5)
 mp1.γ = 2 + 1/(1-mp1.η)
-np1 = NumPar(mp1,nx=100,nsav=150,nq=31,nα=101,nh=1,xmax=10)
+np1 = NumPar(mp1,nx=100,nsav=150,nq=5,nα=101,nh=1,xmax=10)
 MS1 = ModelSolution(mp1,np1)
 @time SolveModel!(MS1)
 
@@ -21,17 +21,15 @@ np2 = NumPar(mp1,nx=np1.nx,nsav=np1.nsav,nq=np1.nq,nα=np1.nα,nh=1,xmax=np1.xma
 mp2 = ModPar(q=mp1.q,ψ=mp1.ψ,xstar=mp1.xstar,η =mp1.η,γ = mp1.γ)
 np2 = NumPar(mp2,nx=np1.nx,nsav=np1.nsav,nq=np1.nq,nα=np1.nα,nh=2,xmax=np1.xmax)
 
-np2.hgrd[1] = 0.5
+np2.hgrd[1] = 1.
 np2.hgrd[2] = 3.0
 MS2 = ModelSolution(mp2,np2)
 @time SolveModel!(MS2)
 
-# np.πrs[1] = 0.00
-# np.πrs[:] = np.πrs/sum(np.πrs)
-# sum(np.πrs.*np.rsgrd)
-# sum(np.πrs.*np.rsgrd.^2 .- sum(np.πrs.*np.rsgrd)^2)
-# MS3 = ModelSolution(mp,np)
-# @time SolveModel!(MS3)
+np2.πrs[1] += 0.005
+np2.πrs[:] = np2.πrs/sum(np.πrs)
+MS3 = ModelSolution(mp2,np2)
+@time SolveModel!(MS3)
 ##
 fig = Figure()
 g2 = fig[1,1] = GridLayout()
@@ -63,13 +61,38 @@ ylims!(ax1,-0.05,1.05)
 lines!(ax2,MS2.np.xgrd, MS1.α[:,1], linestyle=:dash,color=:gray,)
 lines!(ax2,MS2.np.xgrd, MS2.α[:,1], color=:orange)
 ylims!(ax2,-0.05,1.05)
-text!(ax2, "↑\n Gambling\n for housing", position=(3.2,0.2),align=(:right,:center))
-text!(ax2, "Stay safe \nfor housing\n↓", position=(3.8,0.2),align=(:left,:center))
-text!(ax2, "Gradually re-enter\n↓", position=(4.7,0.8),align=(:left,:center))
+# text!(ax2, "↑\n Gambling\n for housing", position=(3.2,0.2),align=(:right,:center))
+# text!(ax2, "Stay safe \nfor housing\n↓", position=(3.8,0.2),align=(:left,:center))
+# text!(ax2, "Gradually re-enter\n↓", position=(4.7,0.8),align=(:left,:center))
 
 lines!(ax3,MS2.np.xgrd, MS2.α[:,1],)
-# lines!(ax3,MS2.np.xgrd, MS3.α[:,1],)
+lines!(ax3,MS2.np.xgrd, MS3.α[:,1],)
 ylims!(ax3,-0.05,1.05)
 
 fig
 ## Run-code
+
+fig = Figure()
+ax1 = Axis(fig[1,1])
+MS = MS1
+hitp = LinearInterpolation(MS.np.xgrd,MS2.h[:,2],extrapolation_bc=Flat())
+
+probh2 = fill(-1.0,MS.np.nx)
+for ix in eachindex(MS.np.xgrd)
+  xns = MS.s[ix,1]*(1.0.+MS.np.rsgrd) .+ MS.b[ix,1] *(1.0 + MS.mp.r)
+  probh2[ix] = sum(MS.np.πrs.*(hitp.(xns).==MS2.np.hgrd[2]))
+end
+
+scatter!(ax1,probh2)
+
+MS = MS2
+hitp = LinearInterpolation(MS2.np.xgrd,MS.h[:,2],extrapolation_bc=Flat())
+
+probh2 = fill(-1.0,MS.np.nx)
+for ix in eachindex(MS.np.xgrd)
+  xns = MS.s[ix,1]*(1.0.+MS.np.rsgrd) .+ MS.b[ix,1] *(1.0 + MS.mp.r)
+  probh2[ix] = sum(MS.np.πrs.*(hitp.(xns).==MS2.np.hgrd[2]))
+end
+
+scatter!(ax1,probh2.+1.0)
+fig
