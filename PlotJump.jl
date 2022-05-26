180b10ca9c572d380a9095be819f286cc6a49bdb
Eirik Eylands Brandsaas <eebrandsaas@gmail.com>
2022-05-26 14:55:25 -0400

    Minor changes to plots

--- PlotJump.jl
+++ PlotJump.jl
@@ -8,8 +8,7 @@ include("src/EasyStocks.jl")
 using CairoMakie # For plotting
 
 mp = ModPar(q=0.0,ψ=0.0,xstar=2.0,η=0.)
-np = NumPar(mp,nx=60,nsav=120,nq=15,nα=81,nh=1,ygrd=[1.0,2.0])
-#np.ygrd[2]=1.0 Easy way to remove income
+np = NumPar(mp,nx=60,nsav=120,nq=15,nα=81,nh=1,ygrd=[1.0,1.0])
 MS1 = ModelSolution(mp,np)
 @time SolveModel!(MS1)
 mp = ModPar(q=mp.q,ψ=0.015,xstar=mp.xstar,η =mp.η)
@@ -26,15 +25,23 @@ g2 = fig[1,1] = GridLayout()
 g1 = fig[2,1] = GridLayout()
 
 ax2  = Axis(g1[1,1],xlabel = "Wealth", ylabel = "α")
+ax3  = Axis(g1[2,1],xlabel = "Wealth", ylabel = "s")
 v2 = Axis(g2[1,1], xlabel ="Wealth", ylabel = "Utils")
 v1 = Axis(g2[1,2], xlabel ="Wealth", ylabel = "Utils")
-
 lines!(v1,np.xgrd,MS1.V[:,1,1],linestyle=:dash,color=:gray,label="Benchmark")
 lines!(v1,np.xgrd,MS2.V[:,1,1],color=:orange,label="Housing")
+ylims!(v1,(MS2.V[np.nx÷7,1,1],0))
 
 
 lines!(v2,np.xgrd,MS1.V[:,1,2],linestyle=:dash,color=:gray,)
 lines!(v2,np.xgrd,MS2.V[:,1,2],color=:orange,)
+
+lines!(ax3,MS2.np.xgrd, MS1.s[:,1,1]+MS1.b[:,1,1], linestyle=:dash,color=:gray)
+lines!(ax3,MS2.np.xgrd, MS2.s[:,1,1]+MS2.b[:,1,1], color=:orange)
+lines!(ax3,MS2.np.xgrd, MS1.s[:,1,1], linestyle=:dash,color=:blue)
+lines!(ax3,MS2.np.xgrd, MS2.s[:,1,1],color=:blue)
+lines!(ax3,MS2.np.xgrd, MS1.b[:,1,1], linestyle=:dash,color=:green)
+lines!(ax3,MS2.np.xgrd, MS2.b[:,1,1],color=:green)
 
 lines!(ax2,MS2.np.xgrd, MS1.α[:,1,1], linestyle=:dash,color=:gray)
 lines!(ax2,MS2.np.xgrd, MS2.α[:,1,1], color=:orange)
@@ -53,3 +60,19 @@ Label(g2[1, 2, Top()], "First Period Value Function", valign = :bottom,)
 
 fig
 ## Run-code
+function Prob_xstar(MS)
+    tmp = fill(0.0,MS.np.nx)
+    for ix in eachindex(MS.np.xgrd)
+        b = MS.b[ix,1,1]
+        s = MS.s[ix,1,1]
+        for irs in eachindex(np.πrs)
+            tmp[ix] += np.πrs[irs]*(xp(b,s,mp.r,np.rsgrd[irs]) > MS.mp.xstar)
+        end
+    end
+    return tmp
+end
+
+Prob_xstar(MS3)
+scatter!(ax2,MS2.np.xgrd, Prob_xstar(MS1), linestyle=:dash,color=:gray)
+scatter!(ax2,MS2.np.xgrd, Prob_xstar(MS2), color=:orange)
+fig
